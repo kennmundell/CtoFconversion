28d38cc9afeb2cd2484e6ebab18227d1ab86daaf
kennmundell <64756644+kennmundell@users.noreply.github.com>
2021-06-13 17:39:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+
+# split a string into list
+result = {word:len(word) for word in sentence.split()}
+
+
+print(result)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# code to convert c to f temperatures using dictionary
+weather_f = {day:temp_c*9/5 + 32 for (day, temp_c) in weather_c.items()}
+
+
+print(weather_f)
+
+
+
